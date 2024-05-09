088905c2ad3c023d65c9de1d03835e082a26147e
rSaezP <ro.saezp@duocuc.cl>
2024-05-08 23:14:46 -0400

    se agrega el formulario de registro

---
+++ js/script.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+    $('#Formulario').submit(function(event) {
+        event.preventDefault(); // Evitar que el formulario se envíe automáticamente
+
+
+        // Realizar validaciones
+        var expr = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+       
+        var nombre = $("#nombre").val();
+        var apellido = $("#apellido").val();
+    
+        var celular = $("#celular").val();
+    
+        var email = $("#email").val();
+       
+
+
+        if (nombre == "") {
+            $("#mensaje2").fadeIn();
+            return false;
+        } else {
+            $("#mensaje2").fadeOut();
+        }
+
+        if (apellido == "") {
+            $("#mensaje3").fadeIn();
+            return false;
+        } else {
+            $("#mensaje3").fadeOut();
+        }
+
+       
+
+        if (celular == "") {
+            $("#mensaje5").fadeIn();
+            return false;
+        } else {
+            $("#mensaje5").fadeOut();
+        }
+
+
+        if (email == "" || !expr.test(email)) {
+            $("#mensaje7").fadeIn();
+            return false;
+        } else {
+            $("#mensaje7").fadeOut();
+        }
+
+
+
+        // Si todos los campos son válidos, enviar el formulario
+        return true;
+    });
+});
